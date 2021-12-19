43f41a1b4294df34e2df7daa8c7b9b5ebd6ce10b
enrmarc <e@lessmarcos.com>
2021-12-19 19:39:23 +0100

    Belbo v0.2.1 (with plugins)

--- cmd/belbo/main.go
+++ cmd/belbo/main.go
@@ -11,7 +11,7 @@ import (
 const (
 	RootPath       = "."
 	ConfigFilename = ".belbo.toml"
-	Version        = "v0.2.0"
+	Version        = "v0.2.1"
 )
 
 // DefaultCfg provides default values for a .belbo.toml config file
